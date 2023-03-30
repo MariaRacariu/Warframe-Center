14adcfee547f88a9f9a41b3b6f233e65795aaea0
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2023-03-30 03:36:24 +0200

    finished front page layout and design

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,13 +1,43 @@
-<a href="/">Home page</a>
-@auth
-<div>
-    <span>Welcome {{auth()->user()->name}}</span>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Warframe Center</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
+    <div>
+
+        <h1 class="frontpage-title"><a href="/">Warframe Center</a></h1>
+
+        @auth
+        <div class="auth-navigation">
+            <div class="auth-inline-item">
+                <p class="auth-nav-text"><a href="/dashboard">Dashboard</a></p>
+            </div>
+            <div class="auth-inline-item">
+                <form method="POST" action="/logout">
+                    @csrf
+                    <button type="submit" class="danger-button">Log out</button>
+                </form>
+            </div>
+        </div>
+        @else
+
+        <button class="primary-button login-button"><a href="/login">Log in</a></button>
+        @endauth
+    </div>
+
+<div class="container">
+    <img src="{{ asset('images/warframe-hero-image.jpg') }}" alt="" class="front-page-hero-image">
+    <div class="hero-text">
+        <h1 class="hero-image-text">Create your <br> own Warframe <br>journey</h1>
+        <button class="primary-button hero-image-button"><a href="/createaccount">Sign Up</a></button>
+    </div>
 </div>
-<a href="/dashboard">Dashboard</a>
-<form method="POST" action="/logout">
-    @csrf
-    <button type="submit" style="color:red;">Log out</button>
-</form>
-@else
-<button><a href="/login">Log in</a></button>
-@endauth
+
